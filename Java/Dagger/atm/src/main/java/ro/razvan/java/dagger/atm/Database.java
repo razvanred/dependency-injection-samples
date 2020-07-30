fbfff19b47c643576de83689a0c5540e646ab34b
Răzvan Roșu <razvanred99@gmail.com>
2020-07-30 11:18:42 +0200

    Complete 8th step of the Java Dagger ATM tutorial

---
+++ Java/Dagger/atm/src/main/java/ro/razvan/java/dagger/atm/Database.java
@@ -0,0 +1,36 @@
+package ro.razvan.java.dagger.atm;
+
+import javax.inject.Inject;
+import java.math.BigDecimal;
+import java.util.HashMap;
+import java.util.Map;
+
+public final class Database {
+
+    private final Map<String, Account> accounts = new HashMap<>();
+
+    @Inject
+    public Database() {
+    }
+
+    public Account getAccount(String username) {
+        return accounts.computeIfAbsent(username, Account::new);
+    }
+
+    static final class Account {
+        private final String username;
+        private BigDecimal balance = BigDecimal.ZERO;
+
+        public Account(String username) {
+            this.username = username;
+        }
+
+        public BigDecimal balance() {
+            return balance;
+        }
+
+        public String username() {
+            return username;
+        }
+    }
+}
--- Java/Dagger/atm/src/main/java/ro/razvan/java/dagger/atm/LoginCommand.java
+++ Java/Dagger/atm/src/main/java/ro/razvan/java/dagger/atm/LoginCommand.java
@@ -7,15 +7,18 @@ public class LoginCommand implements SingleArgCommand {
     public static final String COMMAND_KEY_LOGIN = "login";
 
     private final Outputter outputter;
+    private final Database database;
 
     @Inject
-    public LoginCommand(Outputter outputter) {
+    public LoginCommand(Outputter outputter, Database database) {
         this.outputter = outputter;
+        this.database = database;
     }
 
     @Override
-    public Status handleArg(String arg) {
-        outputter.output(arg + " is logged in.");
+    public Status handleArg(String username) {
+        final var account = database.getAccount(username);
+        outputter.output(account.username() + " is logged in with balance: " + account.balance());
         return Status.HANDLED;
     }
 
